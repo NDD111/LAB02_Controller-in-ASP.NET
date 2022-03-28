a9bec7247bd8133716b029b4b49ea4e508168820
NDD111 <ducboss2002@gmail.com>
2022-03-28 23:13:33 +0700

    first commit

---
+++ Lab02_BTTLam/Lab02_BTTLam/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lab02_BTTLam
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Song",
+               url: "Bai-Hat/Danh-Sach-Bai-Hat",
+               defaults: new { controller = "Song", action = "ShowSong" }
+            );
+            routes.MapRoute(
+                name: "Details Song",
+                url: "Bai-Hat/Chi-Tiet/{SongName}",
+                defaults: new { controller = "Song", action = "DetailSong", SongName = (string)null }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Lab02_BTTLam/Lab02_BTTLam/Controllers/SongController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02_BTTLam.Controllers
+{
+    public class SongController : Controller
+    {
+        // GET: Song
+        public ActionResult ShowSong()
+        {
+            return View();
+        }
+        //GET: Action chi tiết bài hát
+        public ActionResult DetailsSong(string SongName, string AuthorName)
+        {
+            ViewBag.Song = SongName;
+            ViewBag.Author = AuthorName;
+            return View();
+        }
+        public ActionResult DownloadSong()
+        {
+            return View();
+        }
+    }
+}
